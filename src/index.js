c9a8b00b3aec0726a4868bb778196dc41c397151
alvaro santos <alvarossmiguel@gmail.com>
2021-05-07 12:21:52 -0300

    global ssafeAreaView

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Constants from "expo-constants";
 
 import Login from "./screens/Login";
+import Entry from "./screens/Entry";
+import { SafeAreaView } from "react-native";
 
 export default function App() {
-  return <Login></Login>;
+  return (
+    <SafeAreaView
+      style={{ marginTop: Constants.statusBarHeight, backgroundColor: "white" }}
+    >
+      <Entry></Entry>
+    </SafeAreaView>
+  );
 }
