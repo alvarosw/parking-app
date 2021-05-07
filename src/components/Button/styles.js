826f81e5884af3fdc78e80993c7a13cb1284ce49
alvaro santos <alvarossmiguel@gmail.com>
2021-05-07 12:05:33 -0300

    button componentized

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Button = styled.TouchableOpacity`
+  justify-content: center;
+  border-radius: 10px;
+`;
+
+export const ButtonText = styled.Text`
+  align-self: center;
+
+  font-weight: 700;
+  font-size: 16px;
+  color: white;
+  /* font-family: "Red Hat Display"; */
+`;
