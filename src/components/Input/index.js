2ad2149f40853a2d345292072fe77c4c2bf94bff
alvaro santos <alvarossmiguel@gmail.com>
2021-05-07 12:20:31 -0300

    add keyboardAvoidingView

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Feather } from "@expo/vector-icons";
 
 import { Container, TextInput } from "./styles";
+import { KeyboardAvoidingView, Platform } from "react-native";
 
 export default function Input({
   icon,
@@ -12,33 +13,42 @@ export default function Input({
   placeholder,
   isPasswordInput,
 }) {
+  let keyboardVerticalOffset;
+
+  if(Platform.OS) 
+    keyboardVerticalOffset = Platform.OS === 'ios' ? 40 : 0;
+
   if (isPasswordInput) {
     return (
-      <Container>
-        <Feather name={icon} size={iconSize} color={iconColor} />
-        <TextInput placeholder={placeholder} secureTextEntry />
-        <Feather
-          name="eye-off"
-          size={iconSize}
-          color={iconColor}
-          style={{ left: "40%" }}
-        />
-        <Feather
-          name="eye"
-          size={iconSize}
-          color={activeIconColor}
-          style={{
-            left: "77%"
-          }}
-        />
-      </Container>
+      <KeyboardAvoidingView behavior='position' keyboardVerticalOffset={keyboardVerticalOffset}>
+        <Container>
+          <Feather name={icon} size={iconSize} color={iconColor} />
+          <TextInput placeholder={placeholder} secureTextEntry />
+          <Feather
+            name="eye-off"
+            size={iconSize}
+            color={iconColor}
+            style={{ left: "40%" }}
+          />
+          <Feather
+            name="eye"
+            size={iconSize}
+            color={activeIconColor}
+            style={{
+              left: "77%",
+            }}
+          />
+        </Container>
+      </KeyboardAvoidingView>
     );
   } else {
     return (
-      <Container>
-        <Feather name={icon} size={iconSize} color={iconColor} />
-        <TextInput placeholder={placeholder} />
-      </Container>
+      <KeyboardAvoidingView behavior='position' keyboardVerticalOffset={keyboardVerticalOffset}>
+        <Container>
+          <Feather name={icon} size={iconSize} color={iconColor} />
+          <TextInput placeholder={placeholder} />
+        </Container>
+      </KeyboardAvoidingView>
     );
   }
 }
