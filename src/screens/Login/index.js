bd310c7053f1eb4e75921b158008a7389d3385d8
alvaro santos <alvarossmiguel@gmail.com>
2021-05-07 12:23:03 -0300

    replace components

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -3,31 +3,31 @@ import { Feather } from "@expo/vector-icons";
 
 import Input from "../../components/Input";
 
+import Button from "../../components/Button";
+
 import {
-  Wrapper,
   Container,
   Header,
   Logo,
   Vector,
   Title,
   Form,
-  Button,
-  ButtonText,
   ForgotPass,
 } from "./styles";
+import { View } from "react-native";
 
 export default function Login() {
   return (
-    <Wrapper>
-      <Container>
-        <Header>
-          <Feather name="chevron-left" size={30} color="#000" />
-          <Logo>LOGO</Logo>
-        </Header>
+    <Container>
+      <Header>
+        <Feather name="chevron-left" size={30} color="#000" />
+        <Logo>LOGO</Logo>
+      </Header>
 
-        <Vector />
-        <Form>
-          <Title>Faça seu login</Title>
+      <Vector />
+      <Form>
+        <Title>Faça seu login</Title>
+        <View style={{ marginBottom: "7%" }}>
           <Input
             placeholder="Email"
             icon="mail"
@@ -42,12 +42,15 @@ export default function Login() {
             isPasswordInput
             activeIconColor="#06C167"
           />
-          <Button>
-            <ButtonText>Iniciar Sessão</ButtonText>
-          </Button>
-          <ForgotPass>Esqueceu-se da palavra-passe?</ForgotPass>
-        </Form>
-      </Container>
-    </Wrapper>
+        </View>
+        <Button
+          label="Iniciar Sessão"
+          color="#142328"
+          width={320}
+          height={44}
+        />
+        <ForgotPass>Esqueceu-se da palavra-passe?</ForgotPass>
+      </Form>
+    </Container>
   );
 }
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -1,10 +1,4 @@
 import styled from "styled-components/native";
-import Constants from 'expo-constants';
-
-export const Wrapper = styled.SafeAreaView`
-  margin-top: ${Constants.statusBarHeight}px;
-  background-color: white;
-`;
 
 export const Container = styled.View`
   background: #010101;
@@ -43,42 +37,22 @@ export const Vector = styled.View`
 
 export const Form = styled.View`
   padding-bottom: 100%;
+  align-items: center;
   z-index: 1;
 `;
 
 export const Title = styled.Text`
   display: flex;
   margin-bottom: 8%;
-  align-self: center;
 
   /* font-family: "Red Hat Display"; */
   font-size: 24px;
   line-height: 32px;
 `;
 
-export const Button = styled.TouchableOpacity`
-  margin: 10% 0 8%;
-  align-self: center;
-  justify-content: center;
-
-  border-radius: 10px;
-  background-color: #142328;
-  width: 320px;
-  height: 44px;
-`;
-
-export const ButtonText = styled.Text`
-  align-self: center;
-
-  font-weight: 700;
-  font-size: 16px;
-  color: white;
-  /* font-family: "Red Hat Display"; */
-`;
-
 export const ForgotPass = styled.Text`
-  align-self: center;
-
+  margin-top: 8%;
+  /* font-family: "Red Hat Display"; */
   color: #06c167;
   font-weight: 700;
 `;
